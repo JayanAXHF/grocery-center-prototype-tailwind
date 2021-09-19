4c9b84ea7466b7f5ddbcce2fcc5157fc4b2096b4
Jayan <sunil.chdry@gmail.com>
2021-09-19 16:24:38 +0530

    Done in one commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import "./App.css";
+import Atta from "./components/Atta.jsx";
+import Navbar from "./components/Navbar";
+import Rice from "./components/Rice.jsx";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Atta />
+            </Route>
+            <Route exact path="/Rice">
+              <Rice />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
